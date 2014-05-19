2339c38bfa9c95ef48b2c87c404a4c848e72d3d2
Mazlan <akusegan@gmail.com>
2014-05-19 15:06:12 +0800

    update 3
    Change directory files to /assets/

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "assets/css/"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "assets/images/"
+javascripts_dir = "assets/css/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
